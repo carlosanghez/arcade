12ba75402d407d668429bea29f8019c9cac9923c
carlosanghez <carlosanghez@gmail.com>
2020-12-25 10:40:52 +0100

    Update pxt.json, main.blocks, main.ts, main.py, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,53 @@
-console.log("hello")
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.powerDown.play()
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.powerUp.play()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    game.splash("Bene, brava!")
+    game.reset()
+})
+game.splash("Benvenuta Giulia")
+game.splash("Acchiappa il gatto!")
 let mySprite = sprites.create(img`
-    . . 2 2 b b b b b . . . . . . . 
-    . 2 b 4 4 4 4 4 4 b . . . . . . 
-    2 2 4 4 4 4 d d 4 4 b . . . . . 
-    2 b 4 4 4 4 4 4 d 4 b . . . . . 
-    2 b 4 4 4 4 4 4 4 d 4 b . . . . 
-    2 b 4 4 4 4 4 4 4 4 4 b . . . . 
-    2 b 4 4 4 4 4 4 4 4 4 e . . . . 
-    2 2 b 4 4 4 4 4 4 4 b e . . . . 
-    . 2 b b b 4 4 4 b b b e . . . . 
-    . . e b b b b b b b e e . . . . 
-    . . . e e b 4 4 b e e e b . . . 
-    . . . . . e e e e e e b d b b . 
-    . . . . . . . . . . . b 1 1 1 b 
-    . . . . . . . . . . . c 1 d d b 
-    . . . . . . . . . . . c 1 b c . 
-    . . . . . . . . . . . . c c . . 
-    `, SpriteKind.Food)
+    . . . . . f f 4 4 f f . . . . . 
+    . . . . f 5 4 5 5 4 5 f . . . . 
+    . . . f e 4 5 5 5 5 4 e f . . . 
+    . . f b 3 e 4 4 4 4 e 3 b f . . 
+    . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+    . f 3 3 e b 3 e e 3 b e 3 3 f . 
+    . f 3 3 f f e e e e f f 3 3 f . 
+    . f b b f b f e e f b f b b f . 
+    . f b b e 1 f 4 4 f 1 e b b f . 
+    f f b b f 4 4 4 4 4 4 f b b f f 
+    f b b f f f e e e e f f f b b f 
+    . f e e f b d d d d b f e e f . 
+    . . e 4 c d d d d d d c 4 e . . 
+    . . e f b d b d b d b b f e . . 
+    . . . f f 1 d 1 d 1 d f f . . . 
+    . . . . . f f b b f f . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+let mySprite2 = sprites.create(img`
+    e e e . . . . e e e . . . . 
+    c d d c . . c d d c . . . . 
+    c b d d f f d d b c . . . . 
+    c 3 b d d b d b 3 c . . . . 
+    f b 3 d d d d 3 b f . . . . 
+    e d d d d d d d d e . . . . 
+    e d f d d d d f d e . b f b 
+    f d d f d d f d d f . f d f 
+    f b d d b b d d 2 f . f d f 
+    . f 2 2 2 2 2 2 b b f f d f 
+    . f b d d d d d d b b d b f 
+    . f d d d d d b d d f f f . 
+    . f d f f f d f f d f . . . 
+    . f f . . f f . . f f . . . 
+    `, SpriteKind.Enemy)
+mySprite2.setPosition(30, 27)
+scene.setBackgroundColor(3)
+game.onUpdateInterval(2000, function () {
+    mySprite2.setPosition(randint(0, 100), randint(0, 100))
+    music.baDing.play()
+})
